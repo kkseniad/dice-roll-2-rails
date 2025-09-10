b8e28e507c92ee81afd68e4c29978ac20089e4a5
Kseniia Kormalova <kormalovak@gmail.com>
2025-09-10 22:07:18 +0000

    defined all methods

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -13,5 +13,34 @@ class PagesController < ApplicationController
     render ({:template => "pages_template/two_six"})
   end
 
+  def roll_two_ten
+    @rolls = []
+
+    2.times do
+      die = rand(1..10)
+      @rolls.push(die)
+    end
+
+    render ({:template => "pages_template/two_ten"})
+  end
+
+  def roll_one_twenty
+    @die = rand(1..20)
+
+    render ({:template => "pages_template/one_twenty"})
+  end
+
+  def roll_five_four
+    @rolls = []
+
+    5.times do
+      die = rand(1..4)
+
+      @rolls.push(die)
+    end
+
+    render ({:template => "pages_template/five_four"})
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,10 @@ Rails.application.routes.draw do
 
   get("/dice/2/6", { :controller => "pages", :action => "roll_two_six"})
 
-  get("/dice/2/10", { :controller => "pages", :action => "roll2d10"})
+  get("/dice/2/10", { :controller => "pages", :action => "roll_two_ten"})
 
-  get("/dice/1/20", { :controller => "pages", :action => "roll1d20"})
+  get("/dice/1/20", { :controller => "pages", :action => "roll_one_twenty"})
 
-  get("/dice/5/4", { :controller => "pages", :action => "roll5d4"})
+  get("/dice/5/4", { :controller => "pages", :action => "roll_five_four"})
 
 end
